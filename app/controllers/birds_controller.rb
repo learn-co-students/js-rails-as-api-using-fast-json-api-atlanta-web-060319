ca8f578e9fb19ea5ab3c7853d364f006e3788d1c
iheartobi <iobidah9@gmail.com>
2019-07-21 11:45:05 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,12 @@
 class BirdsController < ApplicationController
+    def index
+        birds = Bird.all 
+        options = {
+            include: {
+                location: => {:latitude, :longitude}
+            },
+            sighting:
+        }
+        render json: BirdSerializer.new(birds)
+    end 
 end
